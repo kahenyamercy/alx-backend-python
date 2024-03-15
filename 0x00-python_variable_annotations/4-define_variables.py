9ecfb16d71016620947e895e73c96640c5170cd6
kahenyamercy <kahenyamercy5@gmail.com>
2024-03-15 16:36:46 +0300

    define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Defines and annotates variables with specified values.
+"""
+
+# Variable a: an integer with a value of 1
+a: int = 1
+
+# Variable pi: a float with a value of 3.14
+pi: float = 3.14
+
+# Variable i_understand_annotations: a boolean with a value of True
+i_understand_annotations: bool = True
+
+# Variable school: a string with a value of "Holberton"
+school: str = "Holberton"
