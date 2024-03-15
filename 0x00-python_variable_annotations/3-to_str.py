583dfc583f6d706ad0b7acd469d5dba6a1d2827c
kahenyamercy <kahenyamercy5@gmail.com>
2024-03-15 16:33:15 +0300

    change float o string representation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Function to_str which takes a float n as argument
+and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a float.
+
+    Args:
+        n (float): The float to convert to a string.
+
+    Returns:
+        str: The string representation of the float.
+    """
+    return str(n)
