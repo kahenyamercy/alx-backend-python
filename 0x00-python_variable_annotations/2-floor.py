498a7faa104506d3a1475884037b8480a540de2b
kahenyamercy <kahenyamercy5@gmail.com>
2024-03-15 16:17:42 +0300

    floor function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+function floor which takes a float n as 
+argument and returns the floor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """Returns the largest integer not greater than n."""
+    return int(n // 1)
