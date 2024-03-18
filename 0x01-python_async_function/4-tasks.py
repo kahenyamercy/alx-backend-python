f0dae932a46949dd2aa9d6029acdeed87ce62f10
kahenyamercy <kahenyamercy5@gmail.com>
2024-03-18 20:51:21 +0300

    async function that returns a list of delays in ascending order

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,11 +3,11 @@
 
 import asyncio
 
-wait_random = __import__('0-basic_async_syntax').wait_random
-task_wait_randomn = __import__('3-tasks').task_wait_randomn
+
+task_wait_randomn = __import__('0-basic_async_syntax').wait_randomn
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Return list of delays in ascending order"""
-    delays = [task_wait_randomn(max_delay) for _ in range(n)]
-    return [await delay for delay in asyncio.as_completed(delays)]
+    """Executes task_wait_random n times and returns the sorted results."""
+    wait_tasks = [task_wait_random(max_delay) for _ in range(n)]
+    return sorted(await asyncio.gather(*wait_tasks))
